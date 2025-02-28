81293f2bd66707e0516bf0e444328d229dc549fc
Edgar Lira <edgar.jj.29@gmail.com>
2025-02-27 21:47:23 -0300

    remove dependencia de load_dotenv a fim de remover settings da aplicação

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
 from discord import Intents
 from application.listener import Listener
-from settings.config import TOKEN
 
 
 if __name__ == "__main__":
     client = Listener(intents=Intents.all())
-    client.run(TOKEN)
+    client.run(os.environ.get("TOKEN", ""))
