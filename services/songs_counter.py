2ae78f01a156ce63d0575a623bb48d5952d26c70
Edgar Lira <edgar.jj.29@gmail.com>
2025-04-13 16:57:56 -0300

    refactor[songs repository]: inserts na tabela de música agora retorna numero de vezes de música tocadas sem a necessidade de dar um get explicito após insert

--- services/songs_counter.py
+++ services/songs_counter.py
@@ -13,18 +13,16 @@ class SongsCounter:
     def __init__(self, database_provider=Database()) -> None:
         self.db = database_provider
 
-    def add(self, name: str) -> None:
-        song = self.db.query_one_row(
-            select(self.songs.name).where(self.songs.name == name)
-        )
-        if not song:
-            self.db.statement(insert(self.songs).values(name=name, count=1))
-        else:
+    def add(self, name: str) -> int:
+        if song_count := self.get(name):
             self.db.statement(
                 update(self.songs)
                 .where(self.songs.name == name)
                 .values(count=self.songs.count + 1)
             )
+            return song_count + 1
+        self.db.statement(insert(self.songs).values(name=name, count=1))
+        return 1
 
     def get(self, name: str) -> int:
         query = self.db.query_one_row(
